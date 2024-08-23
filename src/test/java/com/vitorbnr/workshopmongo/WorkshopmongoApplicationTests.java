5f4b18d22d622527d073e66f50de25caba7bb6d5
vitorbnr <vitorbernardotech@gmail.com>
2024-08-23 18:13:17 -0300

    Project created

---
+++ src/test/java/com/vitorbnr/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorbnr.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
