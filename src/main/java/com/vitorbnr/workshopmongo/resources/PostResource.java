4fe18c86865d3f76b7c0bb6c746ba86b536a8772
vitorbnr <vitorbernardotech@gmail.com>
2024-08-30 23:51:22 -0300

    Simple query with query methods

--- src/main/java/com/vitorbnr/workshopmongo/resources/PostResource.java
+++ src/main/java/com/vitorbnr/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.vitorbnr.workshopmongo.resources;
 
 import com.vitorbnr.workshopmongo.domain.Post;
+import com.vitorbnr.workshopmongo.resources.util.URL;
 import com.vitorbnr.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -18,5 +21,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
